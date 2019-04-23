2317aff1aef014212d47e2a5289d4cd916848691
ouyangmland <ouyangmland@gmail.com>
2019-04-23 16:36:24 +0800

    update

--- Install.sh
+++ Install.sh
@@ -89,7 +89,7 @@ function Install()
   wget --no-check-certificate -qO "/tmp/lotServer.tar" "https://github.com/ouyangmland/lotServer/raw/master/lotServer.tar"
   tar -xvf "/tmp/lotServer.tar" -C /tmp
   acce_ver=$(acce_check ${KNV})
-  wget --no-check-certificate -qO "${AcceTmp}/etc/apx.lic" "https://www.speedsvip.com/apx.php?mac=${Mac}"
+  wget --no-check-certificate -qO "${AcceTmp}/etc/apx.lic" "https://www.speedsvip.com/keygen.php?mac=${Mac}"
   [ "$(du -b ${AcceTmp}/etc/apx.lic |cut -f1)" -lt '152' ] && Uninstall "Error! I can not generate the Lic for you, Please try again later. "
   echo "Lic generate success! "
   sed -i "s/^accif\=.*/accif\=\"$Eth\"/" "${AcceTmp}/etc/config"
